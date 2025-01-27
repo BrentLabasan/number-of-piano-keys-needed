741fa5e488c924f247895eec4a8049f0b069f376
Brent Labasan <BrentLabasan@gmail.com>
2025-01-27 13:32:36 -0800

    Material UI showing up in UI.

--- src/PianoKey.tsx
+++ src/PianoKey.tsx
@@ -1,12 +1,16 @@
 import './PianoKey.scss';
 import note from './INote';
 
-export default function PianoKey({midiNumber, noteName}: note) {
-    return(
-        <div className="pianoKey">
+import Button from '@mui/material/Button';
+
+export default function PianoKey({ midiNumber, noteName }: note) {
+    return (
+        <Button
+            // className="pianoKey"
+            variant="outlined">
             {String(midiNumber)}
             <br />
             {noteName}
-        </div>
+        </Button>
     );
 }
