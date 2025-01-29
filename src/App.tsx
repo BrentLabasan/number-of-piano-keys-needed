48b5aac4a329bc389483c7b221980db638b2e1fd
Brent Labasan <BrentLabasan@gmail.com>
2025-01-29 14:34:03 -0800

    TODO Replace my code with ChatGPT code

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,72 @@ import './App.css';
 import PianoKey from './PianoKey';
 import note from './INote';
 
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+
+// BOOKMARK work on RangeSelector component
+// React Hook "React.useState" cannot be called at the top level. React Hooks must be called in a React function component or a custom React Hook function
+
+const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
 //  const arrNoteName_letter = new Array(7);
 //  const arrNoteName_letter = ['C', 'D', 'E', 'F', 'G', 'A', 'B', ];
- const arrNoteName_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', ];
- 
+const arrNoteName_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G',];
+
 
 
 const arrNotes: note[] = new Array();
+
+
+
+
+// BOOKMARK Replace my code with ChatGPT code
+
+const midiNotes = [];
+
+const noteNames = [
+  ["C", "C", false],
+  ["C#", "Db", true],
+  ["D", "D", false],
+  ["D#", "Eb", true],
+  ["E", "E", false],
+  ["F", "F", false],
+  ["F#", "Gb", true],
+  ["G", "G", false],
+  ["G#", "Ab", true],
+  ["A", "A", false],
+  ["A#", "Bb", true],
+  ["B", "B", false]
+];
+
+for (let i = 21; i <= 127; i++) {
+  const octave = Math.floor(i / 12) - 1;
+  const noteIndex = i % 12;
+  const [sharp, flat, isBlackKey] = noteNames[noteIndex];
+  const frequency = 440 * Math.pow(2, (i - 69) / 12);
+  
+  midiNotes.push({
+    midi: i,
+    frequency: parseFloat(frequency.toFixed(2)),
+    sharp: `${sharp}${octave}`,
+    flat: `${flat}${octave}`,
+    isBlackKey
+  });
+}
+
+console.log(midiNotes);
+
+
+
+
 
 /*
 0 1 2 3 4 5 6 1 2 3 4 5 6  /// 
@@ -41,6 +100,23 @@ function App() {
   return (
     <div className="App">
       {renderPianoKeys()}
+      <br /><br />
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/RangeSelector.rsx
@@ -0,0 +1,5 @@
+export default RangeSelector() {
+    return (
+        
+    );
+}
